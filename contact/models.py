5888154478429131000facde669fa3622f30b8b4
Breno <brenolimanf@hotmail.com>
2025-11-01 22:33:15 -0300

    criada model Contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = PhoneNumberField(region='BR')
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
