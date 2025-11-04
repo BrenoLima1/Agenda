07ff756d6d9dd2a9799aca8f4cf556a2e23671d1
Breno <brenolimanf@hotmail.com>
2025-11-03 22:04:26 -0300

    criado formulário de criação

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from contact.models import Contact
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+def create(request):
+    context = {
+
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
