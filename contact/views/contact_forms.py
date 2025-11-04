28bc25ef7df93487663ec0fa719fa454e68c35f3
Breno <brenolimanf@hotmail.com>
2025-11-03 22:51:24 -0300

    formulário alterado

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -4,12 +4,28 @@ from contact.models import Contact
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django import forms
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['first_name', 'last_name', 'phone']
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST),
+        }
+
     context = {
-
+        'form': ContactForm(),
     }
-
     return render(
         request,
         'contact/create.html',
